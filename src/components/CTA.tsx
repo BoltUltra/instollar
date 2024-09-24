2629f4845bd8b2d56c63e84e483781e0d19eb389
Mayowa Sunusi Usman <mayowa.u.sunusi@gmail.com>
2024-09-24 12:29:00 +0100

    updates

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import Loading from "./Loading";
 import { useFetchHomePage } from "@/hooks/useFetchPage";
 
+interface HomePage {
+  ctaHeader: string;
+  ctaAltText: string;
+}
+
 const CTA = () => {
   const { homepage, loading, error } = useFetchHomePage();
+
+  // Add type annotation for homepage
+  const homepageWithDefault: HomePage | undefined = homepage;
 
   if (loading) {
     return <Loading />;
@@ -15,9 +23,9 @@ const CTA = () => {
     <section className="cta py-24 md:pb-40 text-white">
       <div className="md:max-w-2xl mx-auto md:px-0 px-5 text-center">
         <h3 className="font-zilla md:text-[52px] text-3xl pb-4">
-          {homepage?.ctaHeader}
+          {homepageWithDefault?.ctaHeader}
         </h3>
-        <p className="md:text-lg mb-8">{homepage?.ctaAltText}</p>
+        <p className="md:text-lg mb-8">{homepageWithDefault?.ctaAltText}</p>
         <button className="bg-secondary text-black px-5 py-3 rounded-full md:text-base text-sm">
           Get Started
         </button>
